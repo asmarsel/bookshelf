6713f65d9bafe6a6f1ab635381a889dbd83ad323
asmarsel <asmarsel@gmail.com>
2020-03-11 18:10:16 +0200

    new commit

---
+++ src/main/java/lv/itlat/bookshelf/persistence/domain/Reservation.java
@@ -0,0 +1,16 @@
+package lv.itlat.bookshelf.persistence.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity(name = "Reservation")
+@Table(name = "RESERVATION")
+public class Reservation implements Serializable {
+
+    @Id
+    @Column(name = "Id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+}
